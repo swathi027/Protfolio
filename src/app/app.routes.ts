0ec465fd248fabb33389b814d1d378847798d84a
swathi027 <swathimendem027@gmail.com>
2024-08-28 11:50:26 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component:AboutComponent },
   { path: 'services', component: ServicesComponent },
